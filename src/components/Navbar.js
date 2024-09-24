ace5a95851647c7fd40cfec8948845db7fef418b
NaushinK <naushink2709@gmail.com>
2024-09-25 00:47:58 +0530

    login logout toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import { Logo_URL } from "./utils";
-const Navbar = () => {
-    return (
-      <div className="NavContainer">
-        <div className="LogoContainer">
-        <img className="logo" src={Logo_URL} alt="Restaurant Logo" />
 
-        </div>
-        <div className="Navitem">
-          <ul className="item">
-            <li>HOME</li>
-            <li>CONTACT US</li>
-            <li>ABOUT US</li>
-            <li>CART</li>
-          </ul>
-        </div>
-      </div>
-    );
+const Navbar = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  console.log("render");
+
+  const toggleButton = () => {
+    setBtnNameReact(prevName => (prevName === "Login" ? "Logout" : "Login"));
   };
 
-  export default Navbar;
+  return (
+    <div className="NavContainer">
+      <div className="LogoContainer">
+        <img className="logo" src={Logo_URL} alt="Restaurant Logo" />
+      </div>
+      <div className="Navitem">
+        <ul className="item">
+          <li>HOME</li>
+          <li>CONTACT US</li>
+          <li>ABOUT US</li>
+          <li>CART</li>
+          <li>
+            <button className="login-btn" onClick={toggleButton}>
+              {btnNameReact}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

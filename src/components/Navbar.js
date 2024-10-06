b9007a95206496c50dd6dddc6fe7cb03c31378f1
NaushinK <naushink2709@gmail.com>
2024-10-07 01:31:47 +0530

    Routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,25 @@
 import { useState } from "react";
 import { Logo_URL } from "./utils";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
-
-  console.log("render");
 
   const toggleButton = () => {
     setBtnNameReact(prevName => (prevName === "Login" ? "Logout" : "Login"));
   };
 
   return (
-    <div className="NavContainer">
+    <nav className="NavContainer">
       <div className="LogoContainer">
         <img className="logo" src={Logo_URL} alt="Restaurant Logo" />
       </div>
       <div className="Navitem">
         <ul className="item">
-          <li>HOME</li>
-          <li>CONTACT US</li>
-          <li>ABOUT US</li>
-          <li>CART</li>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/contact">CONTACT US</Link></li>
+          <li><Link to="/about">ABOUT US</Link></li>
+          <li><Link to="/cart">CART</Link></li> {/* Make it a Link if needed */}
           <li>
             <button className="login-btn" onClick={toggleButton}>
               {btnNameReact}
@@ -28,7 +27,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/navbar";
 import Body from "./components/Body";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider ,Outlet} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -13,26 +13,34 @@ const Applayout = () => {
   return (
     <div className="Main-container">
       <Navbar />
-      <Body />
+    <Outlet />
     </div>
   );
 };
 // console.log(reslist);
-const appRouter=createBrowserRouter([
+const appRouter=createBrowserRouter(
+  [
   {
     path:"/",
     element:<Applayout/>,
+    children:[
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ],
     errorElement:<Error/>
   },
-  {
-    path:"/about",
-    element:<About/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>
-  }
-])
+
+],)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router ={appRouter}/>);
